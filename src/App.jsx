eb352b635bab34bfe2005192b57620110c784657
CharlotteGuo05 <143556274+CharlotteGuo05@users.noreply.github.com>
2025-04-01 19:40:16 -0500

    Fixed the infinite loop by changing from calling to passing a prop.

--- src/App.jsx
+++ src/App.jsx
@@ -18,19 +18,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]} onSquareClick={handleClick} />
-        <Square value={squares[2]} onSquareClick={handleClick} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}  />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}  />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}  />
       </div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]} onSquareClick={handleClick} />
-        <Square value={squares[2]} onSquareClick={handleClick} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(3)}  />
+        <Square value={squares[1]} onSquareClick={() => handleClick(4)}  />
+        <Square value={squares[2]} onSquareClick={() => handleClick(5)}  />
       </div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]} onSquareClick={handleClick} />
-        <Square value={squares[2]} onSquareClick={handleClick} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(6)}  />
+        <Square value={squares[1]} onSquareClick={() => handleClick(7)}  />
+        <Square value={squares[2]} onSquareClick={() => handleClick(8)}  />
       </div>
     </>
   );
